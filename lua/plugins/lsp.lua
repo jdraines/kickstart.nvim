437546ec7d4eeb92f951e378bf89ec398fe15522
John Raines <johndanielraines@github.com>
2026-01-30 16:54:48 -0600

    fix config merge bug

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -201,7 +201,6 @@ return {
 
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, vim.lsp.protocol.make_client_capabilities())
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
       capabilities.textDocument.completion.completionItem.snippetSupport = true
 
       -- Enable the following language servers
